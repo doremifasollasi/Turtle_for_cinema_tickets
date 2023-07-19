6d30b3edad2a2b8370208ad4c47ed1121cd3dae1
doremifasollasi <lena.lenova.2014@gmail.com>
2023-07-19 23:25:01 +0300

    Created a turtle object

--- main.py
+++ main.py
@@ -20,3 +20,12 @@ main_screen.bgcolor("indigo")
 main_screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
 main_screen.setworldcoordinates(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
 main_screen.title("Cinema")
+
+# To create a turtle object in the Turtle window, you need to create an instance of the Turtle class. 
+# After creating a turtle object, you can use methods to move it, draw it, and control its properties, 
+# such as feather color, feather size, movement speed, and more.
+main_turtle = Turtle()
+main_turtle.hideturtle()
+main_turtle.speed(0)
+main_turtle.penup()
+
