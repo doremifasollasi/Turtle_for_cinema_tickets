3b5611693acc585233af4c7ea62347517d1a5d92
doremifasollasi <lena.lenova.2014@gmail.com>
2023-07-19 23:41:07 +0300

    re-commented the program

--- main.py
+++ main.py
@@ -21,23 +21,30 @@ main_screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
 main_screen.setworldcoordinates(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
 main_screen.title("Cinema")
 
-## To create a turtle object in the Turtle window, 
-# you need to create an instance of the Turtle class. 
+## To create a turtle object in the Turtle window, you need to create an instance of the Turtle class. 
 # After creating a turtle object, you can use methods to move it, draw it, and control its properties, 
 # such as feather color, feather size, movement speed, and more.
+
+## Create a turtle object for drawing.
+# main_turtle is a turtle object that is responsible for drawing graphic objects on the screen using the Turtle module. 
+# It will be used to draw pictures, create shapes, lines, etc. In this code, it is also hidden (hideturtle()), 
+# the maximum drawing speed is set (speed(0)), and the pen is raised (penup()) to the turtle did not leave a trace when moving to a new position.
 main_turtle = Turtle()
 main_turtle.hideturtle()
 main_turtle.speed(0)
 main_turtle.penup()
 
-## Using separate objects allows us to control drawing and text output independently of each other. 
-##The corresponding settings (hideturtle(), speed(), penup()) help ensure more efficient use of objects depending on their purpose.
+# Using separate objects allows us to control drawing and text output independently of each other. 
+# The corresponding settings (hideturtle(), speed(), penup()) help ensure more efficient use of objects depending on their purpose.
 
-## Create a shell object for text output
-# main_turtle is a turtle object that is responsible for drawing graphic objects on the screen using the Turtle module. 
-# It will be used to draw pictures, create shapes, lines, etc. In this code, it is also hidden (hideturtle()), 
-# the maximum drawing speed is set (speed(0)), and the pen is raised (penup()) to the turtle did not leave a trace when moving to a new position.
+## Create a shell object for text output.
+# main_writer is another shell object, but with a different purpose. 
+# It is used to display text on the screen. Normally, shells are used to draw graphic objects, 
+# but in this case, using main_writer, we can add text elements to our drawing. 
+# It also hides the turtle (hideturtle()), sets the maximum drawing speed (speed(0)), 
+# and raises the pen (penup()) so that the turtle doesn't leave a trail when it moves to a new position.
 main_writer = Turtle()
 main_writer.hideturtle()
 main_writer.speed(0)
 main_writer.penup()
+
